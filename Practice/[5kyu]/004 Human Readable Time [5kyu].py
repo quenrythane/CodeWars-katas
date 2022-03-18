a75e35b62a8305f6b0a9986f6c592833cbda8997
quenrythane <artur.babinski.g@gmail.com>
2022-03-18 11:46:13 +0100

    next kata 004

---
+++ Practice/[5kyu]/004 Human Readable Time [5kyu].py
@@ -0,0 +1,23 @@
+# https://www.codewars.com/kata/52685f7382004e774f0001f7
+"""
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
+You can find some examples in the test fixtures.
+"""
+
+
+def make_readable(seconds):
+    import time
+    x = time.gmtime(seconds)
+    return str(f"{('0'+str((x.tm_mday - 1) * 24 + x.tm_hour))[-2:]}:{('0'+str(x.tm_min))[-2:]}:{('0'+str(x.tm_sec))[-2:]}")
+
+
+print(make_readable(120))
+print(make_readable(359999))
+
+
