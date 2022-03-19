b5b30d5a361bd3622ce88a17b82dc8c20fa6bb21
quenrythane <artur.babinski.g@gmail.com>
2022-03-19 20:40:14 +0100

    kata 005

--- Practice/[5kyu]/00.py
+++ Practice/[5kyu]/00.py
@@ -9,4 +9,19 @@ ml = [1, 2, 3, 4]
 
 print(ml and 4)
 
+x = {1, 2}
+print(type(x))
+
+def xyz(arr):
+    arr.pop()
+    return arr
+
+xd = {'NORTH', 'SOUTH'}
+dx = {'NORTH', 'SOUTH'}
+
+for i in range(10000):
+    if not xd == dx:
+        print('False')
+print("coś")
+
 
