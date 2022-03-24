15ca8197cabc8244468abf8007bec4ef8de38325
quenrythane <artur.babinski.g@gmail.com>
2022-03-24 19:29:10 +0100

    kata 7 done

--- Practice/[5kyu]/00.py
+++ Practice/[5kyu]/00.py
@@ -1,31 +1,4 @@
-"""
-for i in range(20):
-    x = ("00" + str(i))[-3:]
-    open(f'{x}.py', 'w')
-"""
+word = "abba"
 
-ml = [1, 2, 3, 4]
-# ml[-1] # = 4
-
-print(ml and 4)
-
-x = {1, 2}
-print(type(x))
-
-def xyz(arr):
-    arr.pop()
-    return arr
-
-xd = {'NORTH', 'SOUTH'}
-dx = {'NORTH', 'SOUTH'}
-
-for i in range(10000):
-    if not xd == dx:
-        print('False')
-print("coś")
-
-
-print(bool(-1))
-print(bool(1))
-print(bool(0))
-
+sorted_word = sorted([c for c in word])
+print(sorted_word)
